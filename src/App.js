172019afea47ba163eff78859e69bcc6b51f9004
twins200202 <twins200202@gmail.com>
2020-06-18 15:08:33 +0900

    함수에서 라이프 사이클 구현하기 - useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
@@ -10,7 +10,8 @@ function App() {
     </div>
   );
 }
-
+let funcStyle = 'color:blue';
+let funcId = 0;
 function FuncComp(props) {
   let numberState = useState(props.initNumber);
   let number = numberState[0];
@@ -22,12 +23,23 @@ function FuncComp(props) {
 
   let [_date, setDate] = useState((new Date()).toString());
 
+  useEffect(function() {
+    console.log('%cfunc => useEffect (componentDidMount & componentDidUpdate) ' + (++funcId), funcStyle);  
+    document.title = number + ' : ' + _date;
+  });
+
+  console.log('%cfunc => render ' + (++funcId), funcStyle);
   return (
     <div className="container">
       <h2>function style component</h2>
       <p>Number : {number}</p>
       <p>Date : {_date}</p>
       <input type="button" value="random" onClick={
+          function() {
+            setNumber(Math.random());
+          }
+        }></input>
+      <input type="button" value="date" onClick={
           function() {
             setDate((new Date()).toString());
           }
@@ -36,12 +48,30 @@ function FuncComp(props) {
   )
 }
 
+let classStyle = 'color:red';
 class ClassComp extends React.Component {
   state = {
     number: this.props.initNumber,
     date:(new Date()).toString()
   }
+  componentWillMount() {
+    console.log("%cclass => componentWillMount", classStyle);
+  }
+  componentDidlMount() {
+    console.log("%cclass => componentDidlMount", classStyle);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("%cclass => shouldComponentUpdate", classStyle);
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log("%cclass => componentWillUpdate", classStyle);
+  }
+  componentDidUpdate(nextProps, nextState) {
+    console.log("%cclass => componentDidUpdate", classStyle);
+  }
   render() {
+    console.log("%cclass => render", classStyle);
     return (
       <div className="container">
         <h2>class style component</h2>
